ae12aa32ac78a71945260a00e89efd300b3fa9e0
jhw <justin84602@gmail.com>
2024-04-11 12:22:58 +0800

    fix: filter session

--- src/hooks/useSessions.ts
+++ src/hooks/useSessions.ts
@@ -1,11 +1,24 @@
-import { useContext } from "react";
+import { useContext, useMemo } from "react";
 import { SessionContext } from "../providers/SessionContext";
-import {type SessionType} from "../types";
+import { type SessionType } from "../types";
+import { useKernelClient } from "./useKernelClient";
 
 export type useSessionsReturnType = SessionType | null;
 
 export function useSessions(): useSessionsReturnType {
+  const { address } = useKernelClient();
   const { sessions } = useContext(SessionContext);
 
-  return sessions;
+  const accountSession = useMemo(() => {
+    if (!sessions) return null;
+    return Object.entries(sessions)
+      .filter(([key, session]) => session.smartAccount === address)
+      .reduce((acc: SessionType, [key, session]) => {
+        acc[key as `0x${string}`] = session;
+        return acc;
+      }, {});
+  }, [sessions, address]);
+
+
+  return accountSession;
 }
